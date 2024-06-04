bce2f8204bb5ceff746f3e0df36023b90a82b8a2
MeatTom <meattom@yandex.ru>
2024-06-05 00:48:38 +0300

    changed env

--- server/models/db.js
+++ server/models/db.js
@@ -1,10 +1,16 @@
 const { Sequelize } = require('sequelize');
 const { DataTypes } = require('sequelize');
 
-const sequelize = new Sequelize('socks_store', 'postgres', 'mitron16', {
-    host: 'localhost',
-    dialect: 'postgres',
-});
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'postgres',
+    }
+);
 
 const VerificationCode = sequelize.define('VerificationCode', {
     email: {
