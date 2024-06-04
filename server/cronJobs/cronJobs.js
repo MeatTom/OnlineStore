fbdcd1b49a2dddb9feab5212808780a77d54bc0e
MeatTom <meattom@yandex.ru>
2024-06-05 02:10:30 +0300

    small changes

--- server/cronJobs/cronJobs.js
+++ server/cronJobs/cronJobs.js
@@ -40,7 +40,7 @@ const createDatabaseBackup = () => {
     const pgConfig = {
         user: process.env.DB_USER,
         host: process.env.DB_HOST,
-        database: process.env.DB_DATABASE,
+        database: process.env.DB_NAME,
         password: process.env.DB_PASSWORD,
         port: process.env.DB_PORT
     };
